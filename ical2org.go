378037b4ac4322bf666df1dca9ed9d1518ac486f
Sheda <sheda@fsfe.org>
2016-11-21 23:29:18 +0100

    Testing jordic/goics

--- ical2org.go
+++ ical2org.go
@@ -1,23 +1,80 @@
 package main
 
-import "github.com/laurent22/ical-go/ical"
+import "github.com/jordic/goics"
 import "io/ioutil"
 import "log"
 import "os"
+import "strings"
+import "time"
+import "fmt"
+
+type Event struct {
+	Start, End  time.Time
+	ID, Summary string
+}
+
+type Events []Event
+
+func (e *Events) ConsumeICal(c *goics.Calendar, err error) error {
+	eventCounter := 0
+	noUidCounter := 0
+	for _, el := range c.Events {
+		node := el.Data
+		// value, ok := node["DTSTART"]
+		// log.Output(0, fmt.Sprintf("value=%v ok=%v", value, ok))
+		// if ok {
+		// 	dtstart, err := value.DateDecode()
+		// 	if err != nil {
+		// 		return err
+		// 	}
+		// }
+		// value, ok = node["DTEND"]
+		// log.Output(0, fmt.Sprintf("value=%v ok=%v", value, ok))
+		// if ok {
+		// 	dtend, err := value.DateDecode()
+		// 	if err != nil {
+		// 		return err
+		// 	}
+		// }
+		value, ok := node["UID"]
+		if !ok {
+			noUidCounter++
+		}
+		uid := value.Val
+
+		value, ok = node["SUMMARY"]
+		if !ok {
+			log.Printf("uid=%v status=error what=\"missing/invalid summary\"\n", uid)
+			continue
+		}
+		summary := value.Val
+		log.Printf("uid=%v status=ok summary=\"%v\"\n", uid, summary)
+
+		// A valid event must have at least both an UID and a summary.
+
+		eventCounter++
+
+		fmt.Printf("* %s\n", summary)
+		fmt.Println("  :PROPERTIES")
+		fmt.Printf("  :UID: %s\n", uid)
+		fmt.Println("  :END")
+	}
+	log.Printf("stats: events=%v no-uid=%v\n", eventCounter, noUidCounter)
+	return nil
+}
 
 func main() {
-	icalData, err := ioutil.ReadAll(os.Stdin)
+	fileContent, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
-		log.Fatalf("Unable to read from stdin")
+		log.Fatal("Unable to read from stdin")
 		os.Exit(65) // EX_DATAERR, see FreeBSD's sysexits(3),
 	}
-	// log.Println("Data:")
-	// log.Println(icalData)
-	icalNode, err := ical.ParseCalendar(string(icalData))
+
+	decoder := goics.NewDecoder(strings.NewReader(string(fileContent)))
+	events := Events{}
+	err = decoder.Decode(&events)
 	if err != nil {
-		log.Fatalf("Parse error")
+		log.Fatal("Can't decode stdin")
 		os.Exit(65) // EX_DATAERR, see FreeBSD's sysexits(3),
 	}
-	log.Println("Node:")
-	log.Println(icalNode)
 }
