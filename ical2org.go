67f059737e6c3a52766dc5dd3d7f0d7cc509627e
Sheda <sheda@fsfe.org>
2016-11-24 22:23:50 +0100

    Prepare to filter events out of our time window

--- ical2org.go
+++ ical2org.go
@@ -15,6 +15,8 @@ type Event struct {
 
 type Events []Event
 
+const TwoWeeks = 24 * 14 // 2 weeks = 24x14 = 336 hours
+
 func (e *Events) ConsumeICal(c *goics.Calendar, err error) error {
 	fmt.Println("-*- eval: (auto-revert-mode 1); -*-")
 
@@ -35,9 +37,9 @@ func (e *Events) ConsumeICal(c *goics.Calendar, err error) error {
 			log.Printf("ERROR error-msg=\"missing start date (dtstart)\" uid=%v\n", uid)
 			continue
 		}
-		dtStart, err := value.DateDecode()
+		start, err := value.DateDecode()
 		if err != nil {
-			log.Printf("ERROR error-msg=\"unable to decode start date (dtstart)\" uid=%v\n", uid)
+			log.Printf("ERROR error-msg=\"unable to decode start date (DTSTART)\" uid=%v\n", uid)
 			continue
 		}
 
@@ -47,17 +49,47 @@ func (e *Events) ConsumeICal(c *goics.Calendar, err error) error {
 			continue
 		}
 		summary := value.Val
-		log.Printf("OK uid=%v summary=\"%v\" dtStart=%v\n", uid,
-			summary, dtStart)
 
-		// A valid event must have at least both an UID and a summary.
+		log.Printf("OK summary=\"%v\" start=\"%v\" since-start=%v uid=%v\n",
+			summary, start, time.Since(start), uid)
+
+		// A valid event must have at least:
+		// - an UID;
+		// - a start date (DTSTART);
+		// and a summary.
 
 		eventCounter++
 
+		var end time.Time
+		value, haveEndDate := node["DTEND"]
+		if haveEndDate {
+			end, err = value.DateDecode()
+			if err != nil {
+				log.Printf("- error-msg=\"unable to decode end date (DTEND)\"\n")
+				continue
+			}
+			log.Printf("- end=\"%v\" since-end=%v\n", end, time.Since(end))
+		}
+
+		// XXX Discard the event if it's out of our time window.
+		//       s       e
+		// -n----[-------]------
+		//   <--> Must be less than 2 weeks in the future.
+		//
+		// ------[-n-----]------
+		//
+		// ------[-------]-----n
+		//                <---> Must be less than 2 weeks in the past.
+
 		fmt.Printf("\n")
 		fmt.Printf("* %s\n", summary)
-		fmt.Printf("  %s\n", orgDate(dtStart))
-		fmt.Printf("  :PROPERTIES\n")
+		if haveEndDate {
+			fmt.Printf("  %s\n", orgDateRange(start, end))
+		} else {
+			fmt.Printf("  %s\n", orgDate(start))
+		}
+
+		fmt.Printf("  :PROPERTIES:\n")
 		fmt.Printf("  :ID: %s\n", uid)
 
 		value, ok = node["LOCATION"]
@@ -88,13 +120,18 @@ func (e *Events) ConsumeICal(c *goics.Calendar, err error) error {
 }
 
 func orgDate(dt time.Time) string {
-	return fmt.Sprintf("<%04d-%02d-%02d %02d:%02d>",
+	return fmt.Sprintf("<%04d-%02d-%02d %s %02d:%02d>",
 		dt.Year(),
 		dt.Month(),
 		dt.Day(),
+		dt.Weekday().String()[0:3],
 		dt.Hour(),
 		dt.Minute(),
 	)
+}
+
+func orgDateRange(start time.Time, end time.Time) string {
+	return fmt.Sprintf("%s--%s", orgDate(start), orgDate(end))
 }
 
 func main() {
