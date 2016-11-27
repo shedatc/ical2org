75dc6d2884d8db7d2d78a9da8f2a3e730ba475ae
Sheda <sheda@fsfe.org>
2016-11-27 21:59:05 +0100

    Discrard events that are out of the current time frame

--- ical2org.go
+++ ical2org.go
@@ -8,78 +8,114 @@ import "strings"
 import "time"
 import "fmt"
 
-type Event struct {
-	Start, End  time.Time
-	ID, Summary string
+type Events struct{}
+
+type TimeFrame struct {
+	Start, End time.Time
 }
 
-type Events []Event
+// Tell if f totally or partially overlap g.
+//
+// Legend: f [] g <>
+//
+//  ---[---]---<------->----- false
+//  ---<--->-----[-------]--- false
+//  ---<-----[------>------]- true
+//  -----[--<--------]--->--- true
+func (f TimeFrame) overlap(g TimeFrame) bool {
+	return f.End.After(g.Start) && f.Start.Before(g.End)
+}
 
-const TwoWeeks = 24 * 14 // 2 weeks = 24x14 = 336 hours
+func (f TimeFrame) contains(t time.Time) bool {
+	return f.Start.Before(t) && f.End.After(t)
+}
+
+const TwoWeeks = 336 * time.Hour // 2 weeks = 2 x 7 x 24 hours
 
 func (e *Events) ConsumeICal(c *goics.Calendar, err error) error {
 	fmt.Println("-*- eval: (auto-revert-mode 1); -*-")
 
-	eventCounter := 0
+	totalCounter := 0
+	processedCounter := 0
 	noUidCounter := 0
+	noStartDateCounter := 0
+	noSummaryCounter := 0
+	invalidStartDateCounter := 0
+	invalidEndDateCounter := 0
+	outOfTimeFrameCounter := 0
+
+	currentTimeFrame := TimeFrame{Start: time.Now().Add(-TwoWeeks), End: time.Now().Add(TwoWeeks)}
+	log.Printf("CONF currentTimeFrame={Start: %v, End: %v}\n",
+		currentTimeFrame.Start, currentTimeFrame.End)
 	for _, el := range c.Events {
 		node := el.Data
 
+		totalCounter++
+
 		value, ok := node["UID"]
 		if !ok {
+			log.Printf("ERROR error-msg=\"missing UID\"\n")
 			noUidCounter++
 			continue
 		}
 		uid := value.Val
+		log.Printf("EVENT uid=\"%v\"\n", uid)
 
 		value, ok = node["DTSTART"]
 		if !ok {
-			log.Printf("ERROR error-msg=\"missing start date (dtstart)\" uid=%v\n", uid)
+			log.Printf("` ERROR error-msg=\"missing start date (DTSTART)\" uid=%v\n", uid)
+			noStartDateCounter++
 			continue
 		}
 		start, err := value.DateDecode()
 		if err != nil {
-			log.Printf("ERROR error-msg=\"unable to decode start date (DTSTART)\" uid=%v\n", uid)
+			log.Printf("` ERROR error-msg=\"unable to decode start date (DTSTART)\" uid=%v\n", uid)
+			invalidStartDateCounter++
 			continue
 		}
+		log.Printf("| start=\"%v\"\n", start)
 
 		value, ok = node["SUMMARY"]
 		if !ok {
-			log.Printf("ERROR error-msg=\"missing summary\" uid=%v\n", uid)
+			log.Printf("` ERROR error-msg=\"missing summary\" uid=%v\n", uid)
+			noSummaryCounter++
 			continue
 		}
 		summary := value.Val
 
-		log.Printf("OK summary=\"%v\" start=\"%v\" since-start=%v uid=%v\n",
-			summary, start, time.Since(start), uid)
-
-		// A valid event must have at least:
-		// - an UID;
-		// - a start date (DTSTART);
-		// and a summary.
-
-		eventCounter++
+		log.Printf("| summary=\"%v\"\n", summary)
 
 		var end time.Time
 		value, haveEndDate := node["DTEND"]
 		if haveEndDate {
 			end, err = value.DateDecode()
 			if err != nil {
-				log.Printf("- error-msg=\"unable to decode end date (DTEND)\"\n")
+				log.Printf("` ERROR error-msg=\"unable to decode end date (DTEND)\"\n")
+				invalidEndDateCounter++
 				continue
 			}
-			log.Printf("- end=\"%v\" since-end=%v\n", end, time.Since(end))
+			log.Printf("| end=\"%v\"\n", end)
+
+			eventTimeFrame := TimeFrame{Start: start, End: end}
+			if currentTimeFrame.overlap(eventTimeFrame) {
+				log.Printf("| KEEP\n")
+			} else {
+				log.Printf("` DISCARD\n")
+				outOfTimeFrameCounter++
+				continue
+			}
+		} else {
+			if currentTimeFrame.contains(start) {
+				log.Printf("| KEEP\n")
+			} else {
+				log.Printf("` DISCARD\n")
+				outOfTimeFrameCounter++
+				continue
+			}
 		}
 
-		// XXX Discard the event if it's out of our time window.
-		//       s       e
-		// -n----[-------]------
-		//   <--> Must be less than 2 weeks in the future.
-		//
-		// ------[-n-----]------
-		//
-		// ------[-------]-----n
-		//                <---> Must be less than 2 weeks in the past.
+		// The event is valid and will be part of the output.
+		processedCounter++
 
 		fmt.Printf("\n")
 		fmt.Printf("* %s\n", summary)
@@ -95,14 +131,14 @@ func (e *Events) ConsumeICal(c *goics.Calendar, err error) error {
 		value, ok = node["LOCATION"]
 		if ok {
 			location := value.Val
-			log.Printf("- location=\"%v\"\n", location)
+			log.Printf("| location=\"%v\"\n", location)
 			fmt.Printf("  :LOCATION: %s\n", location)
 		}
 
 		value, ok = node["STATUS"]
 		if ok {
 			status := value.Val
-			log.Printf("- status=\"%v\"\n", status)
+			log.Printf("| status=\"%v\"\n", status)
 			fmt.Printf("  :STATUS: %s\n", status)
 		}
 
@@ -111,11 +147,15 @@ func (e *Events) ConsumeICal(c *goics.Calendar, err error) error {
 		value, ok = node["DESCRIPTION"]
 		if ok {
 			description := value.Val
-			log.Printf("- description=\"%v\"\n", description)
+			log.Printf("| description=\"%v\"\n", description)
 			fmt.Printf("\n  %s\n", description)
 		}
+		log.Printf("` PROCESSED\n")
 	}
-	log.Printf("STATS events=%v no-uid=%v\n", eventCounter, noUidCounter)
+	log.Printf("STATS total=%v processed=%v no-uid=%v no-start-date=%v no-summary=%v out-of-time-frame=%v invalid-start-date=%v invalid-end-date=%v\n",
+		totalCounter, processedCounter, noUidCounter, noStartDateCounter,
+		noSummaryCounter, outOfTimeFrameCounter,
+		invalidStartDateCounter, invalidEndDateCounter)
 	return nil
 }
 
